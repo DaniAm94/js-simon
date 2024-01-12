f25dd1772b1eea06994d388b958537a09b10ff08
DaniAm94 <dani.amico94@gmail.com>
2024-01-12 17:43:56 +0100

    set timing function

--- js/script.js
+++ js/script.js
@@ -2,16 +2,18 @@ const form = document.querySelector('form');
 const randomFields = document.querySelector('.random-fields')
 const inputFields = document.querySelectorAll('.input-field');
 const button = document.getElementById('button');
+const timerDisplay = document.getElementById('timer');
+const istruction = document.querySelector('h3');
+
+let time;
 
 // Funzioni
 
 const getRandomNumbers = () => {
     const randomNumbers = [];
-    while (randomNumbers.lenght < 5) {
+    while (randomNumbers.length < 5) {
         const randomNumber = Math.floor(Math.random() * 100) + 1;
-        console.log(randomNumber);
         if (!randomNumbers.includes(randomNumber)) {
-
             randomNumbers.push(randomNumber);
         }
     }
@@ -19,8 +21,32 @@ const getRandomNumbers = () => {
 }
 
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    randomFields.innerHTML = '';
+    time = 3;
+    button.disabled = true;
     const randomNumbers = getRandomNumbers();
     console.log(randomNumbers);
+    for (let number of randomNumbers) {
+        const randomField = document.createElement('div');
+        randomField.classList.add('random-number');
+        randomField.innerText = number;
+        randomFields.appendChild(randomField);
+    }
+    const timer = setInterval(() => {
+        timerDisplay.innerText = time--;
+        if (time < 0) {
+            istruction.innerText = 'Indovina i 5 numeri';
+            randomFields.classList.add('d-none');
+            button.disabled = false;
+            button.innerText = 'Conferma';
+            clearInterval(timer);
+            for (let field of inputFields) {
+                field.classList.remove('d-none');
+                field.required = true;
+            }
+        }
+    }, 1000)
 })

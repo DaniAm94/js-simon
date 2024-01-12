70b7239681ca5a2719f3cd89ea2ff03c41392971
DaniAm94 <dani.amico94@gmail.com>
2024-01-12 16:59:24 +0100

    added function to generate random numbers and form event listener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('JS OK!');
+const form = document.querySelector('form');
+const randomFields = document.querySelector('.random-fields')
+const inputFields = document.querySelectorAll('.input-field');
+const button = document.getElementById('button');
+
+// Funzioni
+
+const getRandomNumbers = () => {
+    const randomNumbers = [];
+    while (randomNumbers.lenght < 5) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        console.log(randomNumber);
+        if (!randomNumbers.includes(randomNumber)) {
+
+            randomNumbers.push(randomNumber);
+        }
+    }
+    return randomNumbers;
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const randomNumbers = getRandomNumbers();
+    console.log(randomNumbers);
+})
